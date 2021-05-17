bdbf7fe78cd3fb0421301bf45dfaccd1e637b9a9
luqman <luqmanahmad5149@gmail.com>
2021-05-17 20:49:23 +0800

    layouts-done

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,91 @@
+<footer class="bg-gray-800 py-20 mt-20">
+    <div class="sm:grid grid-cols-3 w-4/5 pb-10 m-auto border-b-2 border-gray-700">
+        <div>
+            <h3 class="text-l sm:font-bold text-gray-100">
+                Pages
+            </h3>
+    
+            <ul class="py-4 sm:text-s pt-4 text-gray-400">
+                <li class="pb-1">
+                    <a href="/">
+                        Home
+                    </a>
+                </li>
+                <li class="pb-1">
+                    <a href="/">
+                        Contact
+                    </a>
+                </li>
+                <li class="pb-1">
+                    <a href="/">
+                        About Us
+                    </a>
+                </li>
+                <li class="pb-1">
+                    <a href="/">
+                        Our Company
+                    </a>
+                </li>
+            </ul>
+        </div>
+        <div>
+            <h3 class="text-l sm:font-bold text-gray-100">
+                Find Us
+            </h3>
+    
+            <ul class="py-4 sm:text-s pt-4 text-gray-400">
+                <li class="pb-1">
+                    <a href="/">
+                        What we do
+                    </a>
+                </li>
+                <li class="pb-1">
+                    <a href="/">
+                        Address
+                    </a>
+                </li>
+                <li class="pb-1">
+                    <a href="/">
+                        Become a Member
+                    </a>
+                </li>
+                <li class="pb-1">
+                    <a href="/">
+                        Rate our services
+                    </a>
+                </li>
+            </ul>
+        </div>
+        <div>
+            <h3 class="text-l sm:font-bold text-gray-100">
+                Follow us
+            </h3>
+    
+            <ul class="py-4 sm:text-s pt-4 text-gray-400">
+                <li class="pb-1">
+                    <a href="/">
+                        Twitter 
+                    </a>
+                </li>
+                <li class="pb-1">
+                    <a href="/">
+                        Facebook
+                    </a>
+                </li>
+                <li class="pb-1">
+                    <a href="/">
+                        Instagram
+                    </a>
+                </li>
+                <li class="pb-1">
+                    <a href="/">
+                        Tik Tok
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+    <p class="w-25 w-4/5 pb-3 m-auto text-xs text-gray-100 pt-6">
+        Copyright 2017-2021 JualBaju.Co | All Rights Reserved
+    </p>
+</footer>
